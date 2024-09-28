6d9ecc6398077158ef9b280f19e7c2ff6f193506
Joselito <josalbarr@alu.edu.gva.es>
2024-09-28 20:40:06 +0200

    pdf added

---
+++ September/Act1/ex1/names.js
@@ -0,0 +1,13 @@
+const names = ["Hola mundo", "Jose Alba Arrufat", "Mr Babu"];
+
+let loop = "";
+for (let i = 0; i <= 1; i++) {
+    loop += "   <li>" + names[i] + "</li>";
+}
+
+document.getElementById("cloop").innerHTML = loop;
+
+
+let showThird = "";
+    alert(names[2]);
+document.getElementById("thirdMessage").innerHTML;
---
+++ September/Act1/ex2/variables.js
@@ -0,0 +1,12 @@
+const number1 = 15;
+const number2 = 26;
+const total12 = number1 + number2;
+
+const variables = [number1+"+"+number2, total12, "Mr Babu"];
+
+let loop = "";
+for (let i = 0; i < variables.length; i++) {
+   loop =+ alert(names[i]);
+}
+
+document.getElementById("cloop").innerHTML;
