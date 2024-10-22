34f8739bae6491f60c910d46426234114980ea4d
Jose <akstarlaantiguaestrellabeta@gmail.com>
2024-10-22 20:40:46 +0200

    started

--- October/Tarea3/Actividad1/script.js
+++ October/Tarea3/Actividad1/script.js
@@ -1,1 +1,11 @@
-// Start to write
+function calcularLetraDni(dni) {
+    let cadena = "TRWAGMYFPDXBNJZSQVHLCKET";
+    dni = parseInt(dni);
+    let posicion = dni % (cadena.length - 1);
+    return alert(cadena[posicion]);
+}
+
+document.getElementById('calcular').addEventListener('click', function() {
+    let dni = document.getElementById('dni');
+    let letra = calcularLetraDni(dni.value);
+})  
