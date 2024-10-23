0dc3c81269b593114af70321b317523c19517518
Joselito <josalbarr@alu.edu.gva.es>
2024-10-23 21:56:21 +0200

    2 and 1 ended

--- October/Tarea3/Actividad1/script.js
+++ October/Tarea3/Actividad1/script.js
@@ -1,19 +1,23 @@
 function getDNInumber(dni) {
     let word = "TRWAGMYFPDXBNJZSQVHLCKET";
     let position = dni % (word.length - 1);
-    
-    if(dni >= 0 && dni < 100000000){
-        return alert(word[position]);
-    }else{
-        return alert('That isnt a valid DNI');
+    if(word[position] !== undefined){
+        return alert(dni + word[position]);
     }
 }
 
 document.getElementById('calc').addEventListener('click', function() {
-    try{
         let dni = document.getElementById('dni');
+        onlyNumbers(dni);
         let word = getDNInumber(dni.value);
-    }catch(err){ // I know, aquí podía utilizar un InputMismathError creo que era para que de error al poner numeros, pero me dio pereza ponerlo a la que me di cuenta que ya lo acabe y no puse el try catch XD
-        alert('Somenthing went wrong in the code and you didnt get the number of the DNI :(');
-    }
 }) 
+
+function onlyNumbers(dni){
+    try{
+        if (isNaN(dni.value)) {
+            throw new InputMismatchError('Input is not a number');
+        } 
+    }catch(err){
+                return alert('That isn\'t a valid DNI');
+        }
+    }
--- October/Tarea3/Actividad2/script.js
+++ October/Tarea3/Actividad2/script.js
@@ -1,1 +1,34 @@
-// Start to write
+document.getElementById('calc').addEventListener('click', function() {
+    // Get the selected values
+    let proValue = document.querySelector('input[name="pro"]:checked').value;
+    let gpValue = document.querySelector('input[name="gp"]:checked').value;
+    let boxValue = document.querySelector('input[name="box"]:checked').value;
+    let refrValue = document.querySelector('input[name="refr"]:checked').value;
+    let monitorValue = document.querySelector('input[name="moni"]:checked').value;
+
+    // Convert values to numbers and calculate the total
+    let total = Number(proValue) + Number(gpValue) + Number(boxValue) + Number(refrValue) + Number(monitorValue);
+
+    // Display the total
+    document.getElementById('cost').innerHTML = `
+<br>    
+  <table>  
+    <tr>
+        <th>Processor</th>
+        <th>Box</th>
+        <th>Refrigeration</th>
+        <th>Monitor</th>
+        <th>Graphic Card</th>
+    </tr>
+    <tr>
+        <td>${proValue}</td>
+        <td>${boxValue}</td>
+        <td>${refrValue}</td>
+        <td>${monitorValue}</td>
+        <td>${gpValue}</td>
+    </tr>
+    <tr>
+        <td colspan="2"> <strong>Total</td>
+        <td colspan="3"> ${total}
+  </table>`;      
+});
