6d49ef400b59d89ebef1d2ab83709a7c92c38a55
Joselito <josalbarr@alu.edu.gva.es>
2024-10-23 09:27:48 +0200

    continue per the 2nd exercise

--- October/Tarea3/Actividad1/script.js
+++ October/Tarea3/Actividad1/script.js
@@ -1,14 +1,19 @@
 function getDNInumber(dni) {
     let word = "TRWAGMYFPDXBNJZSQVHLCKET";
     let position = dni % (word.length - 1);
-    return alert(word[position]);
+    
+    if(dni >= 0 && dni < 100000000){
+        return alert(word[position]);
+    }else{
+        return alert('That isnt a valid DNI');
+    }
 }
 
 document.getElementById('calc').addEventListener('click', function() {
-    let dni = document.getElementById('dni');
     try{
+        let dni = document.getElementById('dni');
         let word = getDNInumber(dni.value);
-    }catch(err){
-        alert('Somenthing went wrong in the code and you didnt get the number of the DNI :(')
+    }catch(err){ // I know, aquí podía utilizar un InputMismathError creo que era para que de error al poner numeros, pero me dio pereza ponerlo a la que me di cuenta que ya lo acabe y no puse el try catch XD
+        alert('Somenthing went wrong in the code and you didnt get the number of the DNI :(');
     }
 }) 
