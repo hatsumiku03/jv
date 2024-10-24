8597e1294f0e7a79b6b69fac36a12492ba3919a4
Jose <akstarlaantiguaestrellabeta@gmail.com>
2024-10-24 19:57:12 +0200

    3 ended and 4 per do

--- October/Tarea3/Actividad3/script.js
+++ October/Tarea3/Actividad3/script.js
@@ -1,25 +1,30 @@
-let library = [
-    { title: "The Great Gatsby", year: 1925, isbn: "978-0743273565", author: "F. Scott Fitzgerald" },
-    { title: "To Kill a Mockingbird", year: 1960, isbn: "978-0060935467", author: "Harper Lee" },
-    { title: "1984", year: 1949, isbn: "978-0451524935", author: "George Orwell" },
-    { title: "The Catcher in the Rye", year: 1951, isbn: "978-0316769488", author: "J.D. Salinger" },
-    { title: "Moby-Dick", year: 1851, isbn: "978-1503280786", author: "Herman Melville" }
-];
+function TheLibrary(){
+  return new Promise((arrayOfBooks) => {
+  setTimeout(() => {
+    arrayOfBooks([
+        { title: "The Great Gatsby", year: 1925, isbn: "978-0743273565", author: "F. Scott Fitzgerald" },
+        { title: "To Kill a Mockingbird", year: 1960, isbn: "978-0060935467", author: "Harper Lee" },
+        { title: "1984", year: 1949, isbn: "978-0451524935", author: "George Orwell" },
+        { title: "The Catcher in the Rye", year: 1951, isbn: "978-0316769488", author: "J.D. Salinger" },
+        { title: "Moby-Dick", year: 1851, isbn: "978-1503280786", author: "Herman Melville" }
+      ]);
+    }, 2000);
+  });
+}
 
-var bookList = document.getElementById('booksTable');
-
-let tableContent = ``
-
-for (const item of library) {
-   tableContent += `
+TheLibrary().then((arrayOfBooksValues) => {
+  let bookList = document.getElementById('booksTable');
+  let tableContent = ``;
+  for (const item of arrayOfBooksValues) {
+    tableContent += `
     <tr>
       <td>${item.title}</td>
       <td>${item.year}</td>
       <td>${item.isbn}</td>
       <td>${item.author}</td>
-    </td>
-  
-  `
+    </td>`
 }
-
-bookList.innerHTML += tableContent
+  bookList.innerHTML += tableContent;
+}).catch((error) => {
+  console.error('Error when u try to fetch the books :(', error);
+});
--- October/Tarea3/Actividad4/script.js
+++ October/Tarea3/Actividad4/script.js
@@ -1,1 +1,21 @@
-// Start to write
+    library = [
+        { title: "The Great Gatsby", year: 1925, isbn: "978-0743273565", author: "F. Scott Fitzgerald" },
+        { title: "To Kill a Mockingbird", year: 1960, isbn: "978-0060935467", author: "Harper Lee" },
+        { title: "1984", year: 1949, isbn: "978-0451524935", author: "George Orwell" },
+        { title: "The Catcher in the Rye", year: 1951, isbn: "978-0316769488", author: "J.D. Salinger" },
+        { title: "Moby-Dick", year: 1851, isbn: "978-1503280786", author: "Herman Melville" }
+        ];
+
+
+    let bookList = document.getElementById('booksTable');
+    let tableContent = ``;
+    for (const item of library) {
+    tableContent += `
+    <tr>
+        <td>${item.title}</td>
+        <td>${item.year}</td>
+        <td>${item.isbn}</td>
+        <td>${item.author}</td>
+    </td>`
+}
+    bookList.innerHTML += tableContent;
