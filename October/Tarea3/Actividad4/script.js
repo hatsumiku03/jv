9b97d873e8a75f6ab5cc8f53892f94800afeb574
Joselito <josalbarr@alu.edu.gva.es>
2024-10-25 10:18:48 +0200

    finish 5

--- October/Tarea3/Actividad4/script.js
+++ October/Tarea3/Actividad4/script.js
@@ -1,21 +1,39 @@
-    library = [
-        { title: "The Great Gatsby", year: 1925, isbn: "978-0743273565", author: "F. Scott Fitzgerald" },
-        { title: "To Kill a Mockingbird", year: 1960, isbn: "978-0060935467", author: "Harper Lee" },
-        { title: "1984", year: 1949, isbn: "978-0451524935", author: "George Orwell" },
-        { title: "The Catcher in the Rye", year: 1951, isbn: "978-0316769488", author: "J.D. Salinger" },
-        { title: "Moby-Dick", year: 1851, isbn: "978-1503280786", author: "Herman Melville" }
-        ];
+async function TheLibrary() {
+    return new Promise(arrayOfBooks => {
+        setTimeout(() => {
+            arrayOfBooks([
+                { title: "The Great Gatsby", year: 1925, isbn: "978-0743273565", author: "F. Scott Fitzgerald" },
+                { title: "To Kill a Mockingbird", year: 1960, isbn: "978-0060935467", author: "Harper Lee" },
+                { title: "1984", year: 1949, isbn: "978-0451524935", author: "George Orwell" },
+                { title: "The Catcher in the Rye", year: 1951, isbn: "978-0316769488", author: "J.D. Salinger" },
+                { title: "Moby-Dick", year: 1851, isbn: "978-1503280786", author: "Herman Melville" }
+            ]);
+        }, 2000);
+    });
+}
 
-
+async function showTheBooksOfTheLibrary() {
     let bookList = document.getElementById('booksTable');
     let tableContent = ``;
-    for (const item of library) {
-    tableContent += `
-    <tr>
-        <td>${item.title}</td>
-        <td>${item.year}</td>
-        <td>${item.isbn}</td>
-        <td>${item.author}</td>
-    </td>`
+    
+    try {
+        // This is where yu use the await to wait to finish the execution of the TheLibrary code ;3
+        let arrayOfBooks = await TheLibrary();
+        
+        for (const item of arrayOfBooks) {
+            tableContent += `
+                <tr>
+                    <td>${item.title}</td>
+                    <td>${item.year}</td>
+                    <td>${item.isbn}</td>
+                    <td>${item.author}</td>
+                </tr>`;
+        }
+
+        bookList.innerHTML += tableContent;
+    } catch (error) {
+        console.error('Error when u try to fetch the books :(', error);
+    }
 }
-    bookList.innerHTML += tableContent;
+
+showTheBooksOfTheLibrary();
--- October/Tarea3/Actividad5/script.js
+++ October/Tarea3/Actividad5/script.js
@@ -1,1 +1,31 @@
-// Start to write
+function go() { 
+    showCircle(150, 150, 100).then(div => { 
+      div.classList.add('message-ball'); 
+      div.append("Hola, mundo!"); 
+    }); 
+ 
+ 
+ 
+  } 
+ 
+  function showCircle(cx, cy, radius) { 
+    let div = document.createElement('div'); 
+    div.style.width = 0; 
+    div.style.height = 0; 
+    div.style.left = cx + 'px'; 
+    div.style.top = cy + 'px'; 
+    div.className = 'circle'; 
+    document.body.append(div); 
+ 
+ 
+    return new Promise(resolve => { 
+      setTimeout(() => { 
+// Faltaria definir lss sentencias para ampliar progresivamente la grandaría del circulo (El radio del mismo) 
+ 
+        div.addEventListener('transitionend', function handler() { 
+          div.removeEventListener('transitionend', handler); 
+          resolve(div); 
+        }); 
+      }, 0); 
+    }) 
+  }
